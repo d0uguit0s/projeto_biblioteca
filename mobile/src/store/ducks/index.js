f95e676ff37cbeb264ab26b7913ac8bc657f8bbe
Douglas Silva de Oliveira <douglas.s.o676@gmail.com>
2020-11-27 20:27:20 -0300

    Mudando o tipo de projeto mobile

--- mobile/src/store/ducks/index.js
+++
@@ -1,8 +0,0 @@
-import { combineReducers } from 'redux';
-import dataUserReducer from './dataUser';
-
-const rootReducer = combineReducers({
-	dataUser: dataUserReducer,
-});
-
-export default rootReducer;
--- mobile/src/store/index.js
+++
@@ -1,6 +0,0 @@
-import { createStore } from 'redux';
-import rootReducer from './ducks/index';
-
-const store = createStore(rootReducer);
-
-export default store;
