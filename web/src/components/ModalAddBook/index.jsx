63108bda8761e4a3196b44397b0de7408811c138
Douglas Silva de Oliveira <douglas.s.o676@gmail.com>
2020-11-27 15:57:54 -0300

    Modal sendo utilizada agora da lib "react-materialize"

--- web/src/components/ModalAddBook/index.jsx
+++ web/src/components/ModalAddBook/index.jsx
@@ -1,11 +1,12 @@
 import React, { useState } from 'react';
 import { connect } from 'react-redux';
+import { Modal, Button } from 'react-materialize';
 import axios from 'axios';
-import './style.css';
 import { Creators as saveDataUserActions } from '../../store/ducks/dataUser';
+import 'materialize-css/dist/css/materialize.min.css';
+import './style.css';
 
 function ModalAddBook({ idUser, booksState, addBook }) {
-	const [show, setShow] = useState('modal hide');
 	const [fieldTitle, setFieldTitle] = useState('');
 	const [fieldSynopsis, setFieldSynopsis] = useState('');
 	const [fieldRead, setFieldRead] = useState(false);
@@ -32,93 +33,103 @@ function ModalAddBook({ idUser, booksState, addBook }) {
 				.then(() => {
 					setFieldTitle('');
 					setFieldSynopsis('');
-					setShow('modal hide');
 				})
 				.catch(error => alert(error));
 		}
 	}
 
 	return (
-		<>
-			<a
-				className='waves-effect waves-light btn-large'
-				onClick={() => setShow('modal show')}
-				onKeyPress={() => setShow('modal show')}
-			>
-				<i className='material-icons left'>library_add</i>adicionar um livro
-			</a>
-
+		<Modal
+			actions={[
+				<Button flat modal='close' node='button' waves='red'>
+					Cancelar
+				</Button>,
+				<Button
+					flat
+					modal='close'
+					node='button'
+					waves='green'
+					onClick={handleSubmit}
+				>
+					Adicionar
+				</Button>,
+			]}
+			bottomSheet={false}
+			fixedFooter={false}
+			header='Insira as informações do livro'
+			id='Modal-0'
+			open={false}
+			options={{
+				dismissible: false,
+				endingTop: '10%',
+				inDuration: 250,
+				onCloseEnd: null,
+				onCloseStart: null,
+				onOpenEnd: null,
+				onOpenStart: null,
+				opacity: 0.5,
+				outDuration: 250,
+				preventScrolling: true,
+				startingTop: '4%',
+			}}
+			//   root={[object HTMLBodyElement]}
+			trigger={
+				<Button large node='button'>
+					<i className='material-icons left'>library_add</i>Adicionar um
+					livro
+				</Button>
+			}
+		>
 			<form className='col s12'>
-				<div id='modal1' className={show}>
-					<div className='modal-content'>
-						<h4>Insira as informações do livro</h4>
-						<div className='row'>
-							<div className='input-field col s12'>
-								<i className='material-icons prefix'>book</i>
-								<input
-									id='title'
-									type='text'
-									className='validate'
-									value={fieldTitle}
-									onChange={e => setFieldTitle(e.target.value)}
-								/>
-								<label htmlFor='title'>Título</label>
-							</div>
-						</div>
-						<div className='row'>
-							<div className='input-field col s12'>
-								<i className='material-icons prefix'>message</i>
-								<textarea
-									id='textarea1'
-									className='materialize-textarea'
-									value={fieldSynopsis}
-									onChange={e => setFieldSynopsis(e.target.value)}
-								/>
-								<label htmlFor='textarea1'>Sinopse</label>
-							</div>
-						</div>
-
-						<p>
-							<label>
-								<input
-									name='read'
-									type='radio'
-									onChange={() => setFieldRead(true)}
-								/>
-								<span>Já li</span>
-							</label>
-						</p>
-						<p>
-							<label>
-								<input
-									name='read'
-									type='radio'
-									onChange={() => setFieldRead(false)}
-									checked
-								/>
-								<span>Ainda vou ler</span>
-							</label>
-						</p>
-					</div>
-					<div className='modal-footer'>
-						<a
-							className='modal-close waves-effect waves-green btn-flat'
-							onClick={() => setShow('modal hide')}
-							onKeyPress={() => setShow('modal hide')}
-						>
-							Cancelar
-						</a>
-						<a
-							className='modal-close waves-effect waves-green btn-flat'
-							onClick={handleSubmit}
-							onKeyPress={handleSubmit}
-						>
-							Adicionar
-						</a>
+				<div className='row'>
+					<div className='input-field col s12'>
+						<i className='material-icons prefix'>book</i>
+						<input
+							id='title'
+							type='text'
+							className='validate'
+							value={fieldTitle}
+							onChange={e => setFieldTitle(e.target.value)}
+						/>
+						<label htmlFor='title'>Título</label>
 					</div>
 				</div>
+				<div className='row'>
+					<div className='input-field col s12'>
+						<i className='material-icons prefix'>message</i>
+						<textarea
+							id='textarea1'
+							className='materialize-textarea'
+							value={fieldSynopsis}
+							onChange={e => setFieldSynopsis(e.target.value)}
+						/>
+						<label htmlFor='textarea1'>Sinopse</label>
+					</div>
+				</div>
+
+				<p>
+					<label>
+						<input
+							name='read'
+							type='radio'
+							onChange={() => setFieldRead(true)}
+						/>
+						<span>Já li</span>
+					</label>
+				</p>
+				<p>
+					<label>
+						<input
+							name='read'
+							type='radio'
+							onChange={() => setFieldRead(false)}
+							checked
+						/>
+						<span>Ainda vou ler</span>
+					</label>
+				</p>
 			</form>
-		</>
+		</Modal>
 	);
 }
 
