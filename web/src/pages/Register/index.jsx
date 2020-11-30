2587f5b24e1ab48c099ee1515fc3e1adc3f5a4ba
Andrey Sena <60768258+andreysena@users.noreply.github.com>
2020-11-30 14:45:22 -0300

    Adicionando cor de fundo e cor do botão de cadastre-se

--- web/src/pages/Register/index.jsx
+++ web/src/pages/Register/index.jsx
@@ -64,7 +64,7 @@ function Register() {
 
 		if (!errorField) {
 			axios
-				.post('http://192.168.2.4:3333/users', data)
+				.post('http://192.168.0.106:3333/users', data)
 				.then(() => {
 					alert('Usuário criado com sucesso!');
 					history.push('/loading');
@@ -77,109 +77,111 @@ function Register() {
 	}
 
 	return (
-		<ContainerForm>
-			<div className='row'>
-				<form className='col s12'>
-					<div className='row'>
-						<div className='input-field col s6'>
-							<input
-								id='first_name'
-								type='text'
-								className={validationName}
-								value={fieldName}
-								onChange={e => setFieldName(e.target.value)}
-								onBlur={() => validateName()}
-								onKeyUp={() => validateName()}
-							/>
-							<label htmlFor='first_name'>Nome</label>
-							<span
-								className='helper-text'
-								data-error='O nome é obrigatório'
-							/>
+		<div className="bg">
+			<ContainerForm>
+				<div className='row'>
+					<form className='col s12'>
+						<div className='row'>
+							<div className='input-field col s6'>
+								<input
+									id='first_name'
+									type='text'
+									className={validationName}
+									value={fieldName}
+									onChange={e => setFieldName(e.target.value)}
+									onBlur={() => validateName()}
+									onKeyUp={() => validateName()}
+								/>
+								<label htmlFor='first_name'>Nome</label>
+								<span
+									className='helper-text'
+									data-error='O nome é obrigatório'
+								/>
+							</div>
+							<div className='input-field col s6'>
+								<input
+									id='last_name'
+									type='text'
+									className={validationLastName}
+									value={fieldLastName}
+									onChange={e => setFieldLastName(e.target.value)}
+									onBlur={() => validateLastName()}
+									onKeyUp={() => validateLastName()}
+								/>
+								<label htmlFor='last_name'>Sobrenome</label>
+								<span
+									className='helper-text'
+									data-error='O sobrenome é obrigatório'
+								/>
+							</div>
 						</div>
-						<div className='input-field col s6'>
-							<input
-								id='last_name'
-								type='text'
-								className={validationLastName}
-								value={fieldLastName}
-								onChange={e => setFieldLastName(e.target.value)}
-								onBlur={() => validateLastName()}
-								onKeyUp={() => validateLastName()}
-							/>
-							<label htmlFor='last_name'>Sobrenome</label>
-							<span
-								className='helper-text'
-								data-error='O sobrenome é obrigatório'
-							/>
-						</div>
-					</div>
 
-					<div className='row'>
-						<div className='input-field col s12'>
-							<input
-								id='email'
-								type='email'
-								className='validate'
-								value={fieldEmail}
-								onChange={e => setFieldEmail(e.target.value)}
-							/>
-							<label htmlFor='email'>E-mail</label>
-							<span
-								className='helper-text'
-								data-error='Insira um e-mail válido'
-								data-success='E-mail ok'
-							>
-								Validação do email
-							</span>
+						<div className='row'>
+							<div className='input-field col s12'>
+								<input
+									id='email'
+									type='email'
+									className='validate'
+									value={fieldEmail}
+									onChange={e => setFieldEmail(e.target.value)}
+								/>
+								<label htmlFor='email'>E-mail</label>
+								<span
+									className='helper-text'
+									data-error='Insira um e-mail válido'
+									data-success='E-mail ok'
+								>
+									Validação do email
+								</span>
+							</div>
 						</div>
-					</div>
 
-					<div className='row'>
-						<div className='input-field col s6'>
-							<input
-								id='password'
-								type='password'
-								className='validate'
-								value={fieldPassword}
-								onChange={e => setFieldPassword(e.target.value)}
-								onBlur={() => validatePsw()}
-							/>
-							<label htmlFor='password'>Senha</label>
+						<div className='row'>
+							<div className='input-field col s6'>
+								<input
+									id='password'
+									type='password'
+									className='validate'
+									value={fieldPassword}
+									onChange={e => setFieldPassword(e.target.value)}
+									onBlur={() => validatePsw()}
+								/>
+								<label htmlFor='password'>Senha</label>
+							</div>
+							<div className='input-field col s6'>
+								<input
+									id='confirmPassword'
+									type='password'
+									className={validationConfirmPsw}
+									value={fieldConfirmPassword}
+									onChange={e => setFieldConfirmPassword(e.target.value)}
+									onKeyUp={() => validatePsw()}
+								/>
+								<label htmlFor='confirmPassword'>Confirme a senha</label>
+								<span
+									className='helper-text'
+									data-error='As senhas não são iguais'
+									data-success='As senhas estão corretas'
+								>
+									Validação da senha
+								</span>
+							</div>
 						</div>
-						<div className='input-field col s6'>
-							<input
-								id='confirmPassword'
-								type='password'
-								className={validationConfirmPsw}
-								value={fieldConfirmPassword}
-								onChange={e => setFieldConfirmPassword(e.target.value)}
-								onKeyUp={() => validatePsw()}
-							/>
-							<label htmlFor='confirmPassword'>Confirme a senha</label>
-							<span
-								className='helper-text'
-								data-error='As senhas não são iguais'
-								data-success='As senhas estão corretas'
+						<div className='areaBtn'>
+							<button
+								className='customBtn btn waves-effect waves-light'
+								type='submit'
+								name='action'
+								onClick={handleSubmit}
 							>
-								Validação da senha
-							</span>
+								Cadastrar-se
+								<i className='material-icons right'>person_add</i>
+							</button>
 						</div>
-					</div>
-					<div className='areaBtn'>
-						<button
-							className='customBtn btn waves-effect waves-light'
-							type='submit'
-							name='action'
-							onClick={handleSubmit}
-						>
-							Cadastrar-se
-							<i className='material-icons right'>person_add</i>
-						</button>
-					</div>
-				</form>
-			</div>
-		</ContainerForm>
+					</form>
+				</div>
+			</ContainerForm>
+		</div>
 	);
 }
 
