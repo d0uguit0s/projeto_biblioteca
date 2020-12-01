66c6b64ebbad2ea9154a9c514e652f915c082309
Andrey Sena <60768258+andreysena@users.noreply.github.com>
2020-12-01 16:54:36 -0300

    Estilização do header da tela de Livros

--- mobile/src/Routes/StackBooks/index.js
+++ mobile/src/Routes/StackBooks/index.js
@@ -5,10 +5,20 @@ import { createStackNavigator } from '@react-navigation/stack';
 import Books from '../../Pages/Private/Books';
 import DetailsBook from '../../Pages/Private/DetailsBook';
 
+const bgStyles = {
+	backgroundColor: '#1fa566',
+}
+
 function StackBooks() {
 	const PrivateStack = createStackNavigator();
 	return (
-		<PrivateStack.Navigator initialRouteName='Livros'>
+		<PrivateStack.Navigator
+				initialRouteName='Livros'
+				screenOptions={{
+					headerStyle: bgStyles,
+					headerTintColor: '#FFF',
+				}}
+		>
 			<PrivateStack.Screen name='Livros' component={Books} />
 			<PrivateStack.Screen name='Detalhes' component={DetailsBook} />
 		</PrivateStack.Navigator>
