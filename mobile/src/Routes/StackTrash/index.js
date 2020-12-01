71e04d8e276237723b7396291915ecdd7b00d55c
Andrey Sena <60768258+andreysena@users.noreply.github.com>
2020-12-01 16:55:08 -0300

    Estilização do header da tela de Lixeira

--- mobile/src/Routes/StackTrash/index.js
+++ mobile/src/Routes/StackTrash/index.js
@@ -4,10 +4,19 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 import Trash from '../../Pages/Private/Trash';
 
+const bgStyles = {
+	backgroundColor: '#1fa566',
+}
+
 function StackTrash() {
 	const PrivateStack = createStackNavigator();
 	return (
-		<PrivateStack.Navigator>
+		<PrivateStack.Navigator
+			screenOptions={{
+				headerStyle: bgStyles,
+				headerTintColor: '#FFF',
+			}}
+		>
 			<PrivateStack.Screen name='Lixeira' component={Trash} />
 		</PrivateStack.Navigator>
 	);
