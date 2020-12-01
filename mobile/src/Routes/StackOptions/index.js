e8556504612d608713a540256ce0ca5e80c89307
Andrey Sena <60768258+andreysena@users.noreply.github.com>
2020-12-01 16:54:53 -0300

    Estilização do header da tela de Opções

--- mobile/src/Routes/StackOptions/index.js
+++ mobile/src/Routes/StackOptions/index.js
@@ -4,10 +4,19 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 import Options from '../../Pages/Private/Options';
 
+const bgStyles = {
+	backgroundColor: '#1fa566',
+}
+
 function StackOptions() {
 	const PrivateStack = createStackNavigator();
 	return (
-		<PrivateStack.Navigator>
+		<PrivateStack.Navigator
+			screenOptions={{
+				headerStyle: bgStyles,
+				headerTintColor: '#FFF',
+			}}
+		>
 			<PrivateStack.Screen name='Opções' component={Options} />
 		</PrivateStack.Navigator>
 	);
