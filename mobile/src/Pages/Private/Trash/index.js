e341cf93dd6954278df407841d7414bc2ec17a01
Andrey Sena <60768258+andreysena@users.noreply.github.com>
2020-12-01 16:51:51 -0300

    Estilização da tela de Lixeira

--- mobile/src/Pages/Private/Trash/index.js
+++ mobile/src/Pages/Private/Trash/index.js
@@ -1,8 +1,9 @@
 import React from 'react';
 
-import { FlatList } from 'react-native';
+import { FlatList, View } from 'react-native';
 import { ListItem } from 'react-native-elements';
 import { connect } from 'react-redux';
+import { styles } from './style';
 
 // import { styles } from './style';
 
@@ -10,16 +11,21 @@ function Trash({ dataUser }) {
 	const renderDeletedBook = ({ item }) => {
 		if (item.deleted)
 			return (
-				<ListItem bottomDivider>
-					<ListItem.Content>
-						<ListItem.Title>{item.title}</ListItem.Title>
-						<ListItem.Subtitle>
-							{item.synopsis.length > 35
-								? item.synopsis.slice(0, 35).concat('...')
-								: item.synopsis}
-						</ListItem.Subtitle>
-					</ListItem.Content>
-				</ListItem>
+				<View style={styles.container}>
+					<ListItem
+						bottomDivider
+						containerStyle={styles.item}
+					>
+						<ListItem.Content>
+							<ListItem.Title style={styles.bookTitle}>{item.title}</ListItem.Title>
+							<ListItem.Subtitle style={styles.bookSubtitle}>
+								{item.synopsis.length > 35
+									? item.synopsis.slice(0, 35).concat('...')
+									: item.synopsis}
+							</ListItem.Subtitle>
+						</ListItem.Content>
+					</ListItem>
+				</View>
 			);
 
 		return null;
@@ -30,6 +36,7 @@ function Trash({ dataUser }) {
 			data={dataUser.books}
 			renderItem={renderDeletedBook}
 			keyExtractor={book => book.id.toString()}
+			style={styles.lista}
 		/>
 	);
 }
--- mobile/src/Pages/Private/Trash/style.js
+++ mobile/src/Pages/Private/Trash/style.js
@@ -1,11 +1,27 @@
 import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
-	container: {
+	lista: {
 		flex: 1,
-		backgroundColor: '#fff',
-		alignItems: 'center',
-		justifyContent: 'center',
+		backgroundColor: '#FFF',
+		padding: 10,
+		margin: 0,
+	},
+	container: {
+		marginTop: 10,
+	},
+	item: {
+		backgroundColor: '#0ebd8263',
+		borderRadius: 15,
+	},
+	bookTitle: {
+		color: '#0299ad',
+		fontSize: 20,
+		fontWeight: 'bold',
+	},
+	bookSubtitle: {
+		color: '#000',
+		fontSize: 15,
 	},
 });
 
