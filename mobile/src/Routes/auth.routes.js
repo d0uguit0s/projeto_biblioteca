7f0aee7f8cdb92008539d623a6194fc088ae7fc5
Andrey Sena <60768258+andreysena@users.noreply.github.com>
2020-12-01 16:53:57 -0300

    Estilização do header da tela de Login

--- mobile/src/Routes/auth.routes.js
+++ mobile/src/Routes/auth.routes.js
@@ -4,10 +4,19 @@ import { createStackNavigator } from '@react-navigation/stack';
 
 import Login from '../Pages/Auth/Login';
 
+const bgStyles = {
+	backgroundColor: '#FFF',
+}
+
 function AuthRoutes() {
 	const AuthStack = createStackNavigator();
 	return (
-		<AuthStack.Navigator>
+		<AuthStack.Navigator
+			screenOptions={{
+				headerStyle: bgStyles,
+				headerTintColor: '#1fa566',
+			}}
+		>
 			<AuthStack.Screen name='Login' component={Login} />
 		</AuthStack.Navigator>
 	);
