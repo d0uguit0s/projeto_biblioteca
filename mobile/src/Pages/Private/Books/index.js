8b38e1255eb8da1ed98e88be21247ecab61e4d50
Andrey Sena <60768258+andreysena@users.noreply.github.com>
2020-12-01 13:59:11 -0300

    Estilização da Home do mobile

--- mobile/src/Pages/Private/Books/index.js
+++ mobile/src/Pages/Private/Books/index.js
@@ -1,34 +1,37 @@
-import React from 'react';
+import React, { useState } from 'react';
 
-import { FlatList } from 'react-native';
+import { FlatList, View } from 'react-native';
 import { ListItem, Icon } from 'react-native-elements';
 import { connect } from 'react-redux';
 
-// import { styles } from './style';
+import { styles } from './style';
 
 function Books({ dataUser, navigation }) {
 	const renderBook = ({ item }) => {
 		if (item.deleted)
 			return (
-				<ListItem
-					bottomDivider
-					onPress={() => navigation.navigate('Detalhes', { book: item })}
-				>
-					<Icon
-						name='check'
-						type='feather'
-						color={item.read ? '#adff2f' : '#808080'}
-					/>
-					<ListItem.Content>
-						<ListItem.Title>{item.title}</ListItem.Title>
-						<ListItem.Subtitle>
-							{item.synopsis.length > 35
-								? item.synopsis.slice(0, 35).concat('...')
-								: item.synopsis}
-						</ListItem.Subtitle>
-					</ListItem.Content>
-					<ListItem.Chevron />
-				</ListItem>
+				<View style={styles.container}>
+					<ListItem
+						bottomDivider
+						onPress={() => navigation.navigate('Detalhes', { book: item })}
+						containerStyle={styles.item}
+					>
+						<Icon
+							name='check'
+							type='feather'
+							color={item.read ? '#adff2f' : '#808080'}
+						/>
+						<ListItem.Content>
+							<ListItem.Title>{item.title}</ListItem.Title>
+							<ListItem.Subtitle>
+								{item.synopsis.length > 35
+									? item.synopsis.slice(0, 35).concat('...')
+									: item.synopsis}
+							</ListItem.Subtitle>
+						</ListItem.Content>
+						<ListItem.Chevron />
+					</ListItem>
+				</View>
 			);
 
 		return null;
@@ -39,6 +42,7 @@ function Books({ dataUser, navigation }) {
 			data={dataUser.books}
 			renderItem={renderBook}
 			keyExtractor={book => book.id.toString()}
+			style={styles.lista}
 		/>
 	);
 }
--- mobile/src/Pages/Private/Books/style.js
+++ mobile/src/Pages/Private/Books/style.js
@@ -1,5 +1,19 @@
 import { StyleSheet } from 'react-native';
 
-const styles = StyleSheet.create({});
+const styles = StyleSheet.create({
+	lista: {
+		flex: 1,
+		backgroundColor: '#FFF',
+		padding: 10,
+		margin: 0,
+	},
+	container: {
+		marginTop: 10,
+	},
+	item: {
+		backgroundColor: '#0ebd8263',
+		borderRadius: 15,
+	},
+});
 
 export { styles };
