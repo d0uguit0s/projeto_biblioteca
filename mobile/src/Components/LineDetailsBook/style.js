c4de9da8519e660d2d05c45d2d6bcbd5d0e9a075
Andrey Sena <60768258+andreysena@users.noreply.github.com>
2020-12-01 21:17:28 -0300

    Ajustes na exibição da informações dos livros cadastrados

--- mobile/src/Components/LineDetailsBook/style.js
+++ mobile/src/Components/LineDetailsBook/style.js
@@ -2,8 +2,8 @@ import { StyleSheet } from 'react-native';
 
 const styles = StyleSheet.create({
 	container: {
-		flexDirection: 'row',
-		justifyContent: 'center',
+		// flexDirection: 'row',
+		alignItems: 'center',
 	},
 
 	label: {
